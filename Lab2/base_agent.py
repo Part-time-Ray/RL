9f96ac318b8c090db38de34ade3878e54e3afeda
Part_time_Ray <wang.ray0321@gmail.com>
2025-10-11 22:43:58 +0800

    initial commit

---
+++ Lab2/base_agent.py
@@ -0,0 +1,262 @@
+import random
+import torch
+import torch.nn as nn
+import numpy as np
+import shutil, os
+import time
+from collections import deque
+from torch.utils.tensorboard import SummaryWriter
+from replay_buffer.replay_buffer import ReplayMemory
+from abc import ABC, abstractmethod
+import imageio
+import cv2
+
+class AtariPreprocessor:
+	"""
+	Preprocesing the state input of DQN for Atari
+	"""    
+	def __init__(self, frame_stack=4):
+		self.frame_stack = frame_stack
+		self.frames = deque(maxlen=frame_stack)
+
+	def preprocess(self, obs):
+		gray = cv2.cvtColor(obs, cv2.COLOR_RGB2GRAY)
+		resized = cv2.resize(gray, (84, 84), interpolation=cv2.INTER_AREA)
+		return resized / 255.
+		# return resized
+
+	def reset(self, obs):
+		frame = self.preprocess(obs)
+		self.frames = deque([frame for _ in range(self.frame_stack)], maxlen=self.frame_stack)
+		return np.stack(self.frames, axis=0)
+
+	def step(self, obs):
+		frame = self.preprocess(obs)
+		self.frames.append(frame)
+		return np.stack(self.frames, axis=0)
+
+if __name__ == '__main__':
+	import gymnasium as gym
+	import ale_py
+	gym.register_envs(ale_py)
+	env = gym.make("ALE/MsPacman-v5", render_mode="rgb_array")
+	preprocessor = AtariPreprocessor(frame_stack=4)
+	observation, info = env.reset()
+	images = []
+	for i in range(100):
+		action = env.action_space.sample()
+		next_observation, reward, terminate, truncate, info = env.step(action)
+		state = preprocessor.step(next_observation)
+	for i, s in enumerate(state):
+		image = np.uint8(s * 255)
+		cv2.imwrite(f"test_{i+1}.png", image)
+
+class DQNBaseAgent(ABC):
+	def __init__(self, config):
+		self.gpu = config["gpu"]
+		self.device = torch.device("cuda" if self.gpu and torch.cuda.is_available() else "cpu")
+		self.total_time_step = 0
+		self.training_steps = int(config["training_steps"])
+		self.batch_size = int(config["batch_size"])
+		self.epsilon = 1.0
+		self.eps_min = config["eps_min"]
+		self.eps_decay = config["eps_decay"]
+		self.eval_epsilon = config["eval_epsilon"]
+		self.warmup_steps = config["warmup_steps"]
+		self.eval_interval = config["eval_interval"]
+		self.eval_episode = config["eval_episode"]
+		self.gamma = config["gamma"]
+		self.update_freq = config["update_freq"]
+		self.update_target_freq = config["update_target_freq"]
+		self.frame_stack = config["frame_stack"]
+		self.video = config["video"]
+		self.network = config["network"]
+		self.game = config['game']
+	
+		self.replay_buffer = ReplayMemory(int(config["replay_buffer_capacity"]))
+		self.preprocessor = AtariPreprocessor(frame_stack=self.frame_stack)
+		self.logdir = os.path.join(config["logdir"], self.game, self.network)
+		if os.path.exists(self.logdir):
+			shutil.rmtree(self.logdir, ignore_errors=True)
+		os.makedirs(self.logdir, exist_ok=True)
+		self.writer = SummaryWriter(self.logdir)
+		self.best_eval_reward = -1
+
+	@abstractmethod
+	def decide_agent_actions(self, observation, epsilon=0.0, action_space=None):
+		### TODO ###
+		# get action from behavior net, with epsilon-greedy selection
+		action = None
+		return action
+	
+	def update(self):
+		# print(self.total_time_step, self.update_freq, self.update_target_freq)
+		if self.total_time_step % self.update_freq == 0:
+			self.update_behavior_network()
+		if self.total_time_step % self.update_target_freq == 0:
+			# print("Update target network")
+			self.update_target_network()
+
+	@abstractmethod
+	def update_behavior_network(self):
+		# sample a minibatch of transitions
+		state, action, reward, next_state, done = self.replay_buffer.sample(self.batch_size, self.device)
+		### TODO ###
+		# calculate the loss and update the behavior network
+		
+
+	def update_target_network(self):
+		self.target_net.load_state_dict(self.behavior_net.state_dict())
+	
+	def epsilon_decay(self):
+		self.epsilon -= (1 - self.eps_min) / self.eps_decay
+		self.epsilon = max(self.epsilon, self.eps_min)
+
+	def train(self):
+		episode_idx = 0
+		while self.total_time_step <= self.training_steps:
+			observation, info = self.env.reset()
+			state = self.preprocessor.reset(observation)
+			episode_reward = 0
+			episode_len = 0
+			episode_idx += 1
+			while True:
+				if self.total_time_step < self.warmup_steps:
+					action = self.decide_agent_actions(state, 1.0, self.env.action_space)
+				else:
+					action = self.decide_agent_actions(state, self.epsilon, self.env.action_space)
+					self.epsilon_decay()
+
+				next_observation, reward, terminate, truncate, info = self.env.step(action)
+				next_state = self.preprocessor.step(next_observation)
+
+				# self.replay_buffer.append(state, action, reward, next_state, terminate.astype(int))
+
+				self.replay_buffer.append(state, [action], [reward], next_state, [int(terminate)])
+
+				if self.total_time_step >= self.warmup_steps:
+					self.update()
+
+				episode_reward += reward
+				episode_len += 1
+				if terminate or truncate:
+					self.writer.add_scalar('Train/Episode Reward', episode_reward, self.total_time_step)
+					self.writer.add_scalar('Train/Episode Len', episode_len, self.total_time_step)
+					print(f"[{self.total_time_step}/{self.training_steps} ({self.total_time_step/self.training_steps*100:.1f}%)]  episode: {episode_idx}  episode reward: {episode_reward}  episode len: {episode_len}  epsilon: {self.epsilon}  learning_rate: {self.lr}")
+					break
+				# observation = next_observation
+				state = next_state
+				self.total_time_step += 1
+
+			if episode_idx % self.eval_interval == 0:
+				# save model checkpoint
+				avg_score = self.evaluate()
+				# self.save(os.path.join(self.writer.log_dir, f"model_{self.total_time_step}_{int(avg_score)}.pth"))
+				self.writer.add_scalar('Evaluate/Episode Reward', avg_score, self.total_time_step)
+
+	def evaluate(self):
+		print("==============================================")
+		print("Evaluating...")
+		all_rewards = []
+		for i in range(self.eval_episode):
+			frames = []
+			observation, info = self.test_env.reset()
+			state = self.preprocessor.reset(observation)
+			total_reward = 0
+			while True:
+				frame = self.test_env.render()
+				frames.append(frame)
+				action = self.decide_agent_actions(state, self.eval_epsilon, self.test_env.action_space)
+				next_observation, reward, terminate, truncate, info = self.test_env.step(action)
+				next_state = self.preprocessor.step(next_observation)
+				total_reward += reward
+				if terminate or truncate:
+					print(f"episode {i+1} reward: {int(total_reward)}")
+					all_rewards.append(total_reward)
+					break
+
+				state = next_state
+				# observation = next_observation
+			if self.video:
+				video_path = os.path.join("video", f"{self.game}", f"{self.network}")
+				os.makedirs(video_path, exist_ok=True)
+				with imageio.get_writer(os.path.join(video_path, f"eval_{i+1}.mp4"), fps=30) as video:
+					for f in frames:
+						height, width = f.shape[:2]
+						new_width = (width // 16) * 16
+						new_height = (height // 16) * 16
+						video.append_data(cv2.resize(f, (new_width, new_height)))
+
+		avg = sum(all_rewards) / self.eval_episode
+		print(f"average score: {avg}")
+		print("==============================================")
+		if avg > self.best_eval_reward:
+			self.best_eval_reward = avg
+			save_path = os.path.join("saved_model", f"{self.game}")
+			os.makedirs(save_path, exist_ok=True)
+			self.save(os.path.join(save_path, f"{self.network}_model.pth"))
+		return avg
+	
+	def evaluate_one_episode(self, seed=42):
+		frames = []
+		observation, info = self.test_env.reset(seed=seed)
+		state = self.preprocessor.reset(observation)
+		total_reward = 0
+		while True:
+			frame = self.test_env.render()
+			frames.append(frame)
+			action = self.decide_agent_actions(state, 0, self.test_env.action_space)
+			next_observation, reward, terminate, truncate, info = self.test_env.step(action)
+			next_state = self.preprocessor.step(next_observation)
+			total_reward += reward
+			if terminate or truncate:
+				print(f"Total reward: {int(total_reward)}")
+				break
+
+			state = next_state
+			# observation = next_observation
+		if self.video:
+			video_path = os.path.join("video", f"{self.game}", "inference")
+			os.makedirs(video_path, exist_ok=True)
+			with imageio.get_writer(os.path.join(video_path, f"{self.network}_eval.mp4"), fps=30) as video:
+				for f in frames:
+					height, width = f.shape[:2]
+					new_width = (width // 16) * 16
+					new_height = (height // 16) * 16
+					video.append_data(cv2.resize(f, (new_width, new_height)))
+		return int(total_reward)
+	
+	# save model
+	def save(self, save_path):
+		if self.parallel_gpu and torch.cuda.device_count() > 1:
+			torch.save(self.behavior_net.module.state_dict(), save_path)
+		else:
+			torch.save(self.behavior_net.state_dict(), save_path)
+
+	# load model
+	def load(self, load_path):
+		self.behavior_net.load_state_dict(torch.load(load_path, weights_only=True))
+
+	# load model weights and evaluate
+	def load_and_evaluate(self, load_path):
+		self.load(load_path)
+		seed_map = {
+			"DDQN": 16143,
+			"DQN": 95291,
+		}
+		self.evaluate_one_episode(seed=seed_map.get(self.network, 42))
+		return
+		max_reward = -float('inf')
+		max_seed = 0
+		for random_seed in random.sample(range(1, 100000), 500):
+			print(f"Random seed: {random_seed}")
+			total_reward = self.evaluate_one_episode(seed=random_seed)
+			print(f"\tTotal reward: {total_reward}")
+			if total_reward > max_reward:
+				max_reward = total_reward
+				max_seed = random_seed
+			print("==============================================")
+		print(f"Best reward {max_reward} under random seed {max_seed}")
+
+
+
---
+++ Lab2/dqn_agent_atari.py
@@ -0,0 +1,98 @@
+import torch
+import torch.nn as nn
+import numpy as np
+from torch.utils.tensorboard import SummaryWriter
+from base_agent import DQNBaseAgent
+from models.atari_model import AtariNetDQN
+from models.dueling_atari_model import AtariNetDQN as DuelingAtariNetDQN
+import gymnasium as gym
+import random
+from gymnasium.vector import AsyncVectorEnv, SyncVectorEnv
+import ale_py
+gym.register_envs(ale_py)
+
+class AtariDQNAgent(DQNBaseAgent):
+	def __init__(self, config):
+		super(AtariDQNAgent, self).__init__(config)
+		### TODO ###
+		# initialize env
+		self.env_name = 'ALE/MsPacman-v5' if self.game == 'Pacman' else 'ALE/Enduro-v5'
+		self.num_envs = config["num_envs"]
+		self.env = gym.make(self.env_name)
+		self.parallel_gpu = config["parallel_gpu"]
+		### TODO ###
+		# initialize test_env
+		self.test_env = gym.make(self.env_name, render_mode="rgb_array")
+
+		# initialize behavior network and target network
+		if self.network == "Dueling":
+			self.behavior_net = DuelingAtariNetDQN(num_classes=self.env.action_space.n, channel=self.frame_stack)
+			self.target_net = DuelingAtariNetDQN(num_classes=self.env.action_space.n, channel=self.frame_stack)
+		else:
+			self.behavior_net = AtariNetDQN(num_classes=self.env.action_space.n, channel=self.frame_stack)
+			self.target_net = AtariNetDQN(num_classes=self.env.action_space.n, channel=self.frame_stack)
+		self.target_net.load_state_dict(self.behavior_net.state_dict())
+		if self.parallel_gpu and torch.cuda.device_count() > 1:
+			print(f"Using {torch.cuda.device_count()} GPUs for DataParallel.")
+			self.behavior_net = nn.DataParallel(self.behavior_net)
+			self.target_net = nn.DataParallel(self.target_net)
+		self.target_net.to(self.device)
+		self.behavior_net.to(self.device)
+		# initialize optimizer
+		self.lr = config["learning_rate"]
+		self.optim = torch.optim.Adam(self.behavior_net.parameters(), lr=self.lr, eps=1.5e-4)
+		
+	def decide_agent_actions(self, observation, epsilon=0.0, action_space=None):
+		### TODO ###
+		# get action from behavior net, with epsilon-greedy selection
+		# print(observation.shape)
+		if random.random() < epsilon:
+			action = action_space.sample()
+		else:
+			if len(observation.shape) == 3:
+				observation = np.expand_dims(observation, axis=0)
+			observation = torch.tensor(observation,device=self.device)
+			with torch.no_grad():
+				action = self.behavior_net(observation).argmax(1).long()[0].cpu()
+				action = int(action)
+
+		# return action
+
+		return action
+
+	def update_behavior_network(self):
+		# sample a minibatch of transitions
+		state, action, reward, next_state, done = self.replay_buffer.sample(self.batch_size, self.device)
+
+		### TODO ###
+		# calculate the loss and update the behavior network
+		# 1. get Q(s,a) from behavior net
+		# 2. get max_a Q(s',a) from target net
+		# 3. calculate Q_target = r + gamma * max_a Q(s',a)
+		# 4. calculate loss between Q(s,a) and Q_target
+		# 5. update behavior net
+
+		if self.network == "DQN":
+			# DQN
+			q_value = self.behavior_net(state).gather(1, action.long())
+			with torch.no_grad():
+				q_next = self.target_net(next_state)
+				q_target = reward + self.gamma * q_next.max(1)[0].unsqueeze(1) * (1 - done)
+		elif self.network == "DDQN" or self.network == "Dueling":
+			# DDQN
+			q_value = self.behavior_net(state).gather(1, action.long())
+			with torch.no_grad():
+				best_action = self.behavior_net(next_state).argmax(dim=-1)
+				q_target = reward + self.gamma * self.target_net(next_state).gather(1, best_action.unsqueeze(1)) * (1-done)
+		else:
+			raise Exception("network error!!!")
+
+		criterion = nn.MSELoss()
+		loss = criterion(q_value, q_target)
+
+		self.writer.add_scalar(f'{self.network}/Loss', loss.item(), self.total_time_step)
+
+		self.optim.zero_grad()
+		loss.backward()
+		self.optim.step()
+	
